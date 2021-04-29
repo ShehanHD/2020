98fb51ece0bf17b2d9439fe19bb94c397ab7a83d
ShehanHD <51677228+ShehanHD@users.noreply.github.com>
2021-04-29 11:58:36 +0200

    .

--- frontend/src/Components/Admin/Admin.js
+++ frontend/src/Components/Admin/Admin.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import { Paper, TextField } from '@material-ui/core';
+import { Paper, TextField, Button, Typography } from '@material-ui/core';
 import './admin.scss';
+import useStyles from '../../Hooks/useStyles';
 
 const Admin = () => {
+    const classes = useStyles()
+
     return (
-        <div id="#admin">
+        <div id="admin">
             <div className="login">
-                <TextField
-                    name="username"
-                    label="Username"
-                    value={""}
-                    onChange={(e) => console.log("a")}
-                />
+                <form className={classes.form} noValidate>
+                    <Typography variant="h4" align={"center"} color="initial">Admin Login</Typography>
+
+                    <TextField
+                        fullWidth
+                        variant="outlined"
+                        margin="normal"
+                        name="username"
+                        label="Username"
+                        value={""}
+                        onChange={(e) => console.log("a")}
+                    />
+
+                    <TextField
+                        fullWidth
+                        variant="outlined"
+                        margin="normal"
+                        name="password"
+                        label="Password"
+                        value={""}
+                    //   onChange={}
+                    />
+
+                    <Button fullWidth margin="normal" className={classes.submit} variant="contained" color="default">
+                        Login
+                    </Button>
+                </form>
             </div>
         </div>
     )
