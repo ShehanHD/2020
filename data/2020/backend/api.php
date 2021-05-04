b5b4da65c676efe6aaa245c4d376c264a53a5c02
ShehanHD <shehanhd@gmail.com>
2021-05-04 14:10:02 +0000

    .

--- data/2020/backend/api.php
+++ data/2020/backend/api.php
@@ -1,11 +1,13 @@
 <?php
+require_once("./src/Shared/AutoReloader.php");
 
 $HTTP_ORIGIN = $_SERVER['HTTP_ORIGIN'];
 
 if (
     $HTTP_ORIGIN == "https://www.wecode.best" ||
     $HTTP_ORIGIN == "https://wecode.best" ||
-    $HTTP_ORIGIN == "http://localhost:3000"
+    $HTTP_ORIGIN == "http://localhost:3000" ||
+    $HTTP_ORIGIN == null
 ) {
 
     header("Access-Control-Allow-Origin: $HTTP_ORIGIN");
@@ -25,28 +27,25 @@ if (
     if ($URL[0] === "2020" && $URL[1] === "backend" && $URL[2] === "api") {
         switch ($controller) {
             case 'site_management':
-                include('./src/Controller/SiteManagementController.php');
                 new SiteManagementController(array_slice($URL, 4), $REQUEST_METHOD, file_get_contents('php://input'));
                 break;
             case 'todo':
-                include('./src/Controller/TodoController.php');
                 new TodoController(array_slice($URL, 4), $REQUEST_METHOD, file_get_contents('php://input'));
                 break;
             case 'verifica1':
-                include('./src/Controller/Verifica1Controller.php');
                 new Verifica1Controller(array_slice($URL, 4), $REQUEST_METHOD, file_get_contents('php://input'));
                 break;
             case 'esercizio1':
-                include('./src/Controller/Esercizio1Controller.php');
                 new Esercizio1Controller(array_slice($URL, 4), $REQUEST_METHOD, file_get_contents('php://input'));
                 break;
             case 'student':
-                include('./src/Controller/StudentController.php');
                 new StudentController(array_slice($URL, 4), $REQUEST_METHOD, file_get_contents('php://input'));
                 break;
             case 'trace':
-                include('./src/Controller/TracerController.php');
-                new TracerController(array_slice($URL, 4), $REQUEST_METHOD, file_get_contents('php://input'), $_SERVER['HTTP_X_REAL_IP']);
+                new TracerController(array_slice($URL, 4), $REQUEST_METHOD, file_get_contents('php://input'), $HTTP_ORIGIN); //$_SERVER['HTTP_X_REAL_IP']
+                break;
+            case 'auth':
+                new UserController(array_slice($URL, 4), $REQUEST_METHOD, file_get_contents('php://input'), $HTTP_ORIGIN); //$_SERVER['HTTP_X_REAL_IP']
                 break;
             default:
                 echo http_response_code(404);
